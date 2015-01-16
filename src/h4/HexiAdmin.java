db14648da5ce5a1248f7c18539d75ea7091e4980
H4ukka <laine.antti.e@gmail.com>
2015-01-16 10:28:57 +0200

    initial commit

--- src/h4/HexiAdmin.java
+++ src/h4/HexiAdmin.java
@@ -1,7 +1,19 @@
 package h4;
 
 /**
+ * Hexicraft Admin Tools
+ * ver 0.1r0
+ *
  * Created by h4 on 16.1.2015.
+ *
+ * Features:
+ *  - None
  */
+
+/**
+ * TODO Write the plugin
+ * TODO Add MySQL Class
+ */
+
 public class HexiAdmin {
 }
